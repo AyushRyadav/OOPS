bc225b19efb560ca132ebf4c2a051c4ebc0ff6ad
Aayush <ay4503301@gmail.com>
2026-01-08 12:19:32 +0530

    Add files via upload

---
+++ Day-4/do while.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+int num;
+do{
+cout<<"enter a number: ";
+cin>>num;}
+while(num<=100);
+cout<<"you entered a number greatest than hundred"<<num;
+return 0;
+}
